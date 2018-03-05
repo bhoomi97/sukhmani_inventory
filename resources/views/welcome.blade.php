62afbf0ec8ceeb47f298bdc2ee2b3e2787b5cdf2
HimaniPanwar <himanipanwar34@gmail.com>
2018-03-05 16:20:55 +0530

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background: url(".../public/about-img1.jpg");
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
