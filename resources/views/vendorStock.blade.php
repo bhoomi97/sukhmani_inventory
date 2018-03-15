4dec613d78ea611c7681ebbe23efca6f67e9748b
Himanshu Agrawal <himanshuagrawal1998@gmail.com>
2018-03-15 09:13:58 +0530

    fix vendor stock heading

--- resources/views/vendorStock.blade.php
+++ resources/views/vendorStock.blade.php
@@ -12,7 +12,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header" id="warehouse_stock">WareHouse Stock</div>
+                <div class="card-header" id="warehouse_stock">Vendor Stock</div>
 
                 <div class="card-body">
                     @if (session('status'))
